d4fa72e959918088bcc54e09170a2d36df677015
Yevhen Darovykh <jenya.darovykh@gmail.com>
2024-08-25 20:55:15 +0200

    The square root calculation code has been moved to a separate class to match the LeetCode structure

--- Square root/Main.cpp
+++ Square root/Main.cpp
@@ -1,28 +1,9 @@
 #include <iostream>
+#include "Solution.cpp"
 using namespace std;
 
 void main()
 {
-	int x = 2;
-
-	// number of bits to represent x
-	int b = log2(x) + 1;
-	
-	// 2^(log2(x)/2) - initial approximation
-	double xn = 1;
-	for (int i = 0; i < b / 2; i++)
-	{
-		xn *= 2;
-	}
-
-	// Newton's method implementation
-	double prevx = -1;
-	while (prevx != xn)
-	{
-		prevx = xn;
-		xn = 0.5 * (xn + x / xn);
-	}
-
-	cout << static_cast<int>(xn) << endl;
-
+	Solution soulution;
+	cout << soulution.mySqrt(16) << endl;
 }
---
+++ Square root/Solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int mySqrt(int x) {
+		// number of bits to represent x
+		int b = log2(x) + 1;
+
+		// 2^(log2(x)/2) - initial approximation
+		double xn = 1;
+		for (int i = 0; i < b / 2; i++) {
+			xn *= 2;
+		}
+
+		// Newton's method implementation
+		double prevx = -1;
+		while (prevx != xn) {
+			prevx = xn;
+			xn = 0.5 * (xn + x / xn);
+		}
+
+		return static_cast<int>(xn);
+    }
+};
